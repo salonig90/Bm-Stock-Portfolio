e9d65eda7d7c266471aad63501c926a016e6b0cf
Saloni Giratkar <giratkarsaloni1@gmail.com>
2026-03-12 19:54:09 +0530

    Update api.js

--- frontend/Frontend/src/services/api.js
+++ frontend/Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "http://20.198.9.109:8000/api/",
 });
 
 const getNormalizedToken = () => {
